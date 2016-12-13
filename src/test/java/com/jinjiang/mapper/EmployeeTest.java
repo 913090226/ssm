ad85ddccbb8c69a33864500517b4345e2a375f01
jerry <913090226@qq.com>
2016-12-13 23:24:18 +0800

    modify the application.xml

--- src/test/java/com/jinjiang/mapper/EmployeeTest.java
+++ src/test/java/com/jinjiang/mapper/EmployeeTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.jinjiang.utils.JsonUtils;
+
 @ContextConfiguration(locations={"classpath:application.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class EmployeeTest {
@@ -15,6 +17,8 @@ public class EmployeeTest {
 	@Test
 	public void test() {
 		System.out.println(employeeMapper.findAll());
+		System.out.println("------------------------------------------------------------------");
+		System.out.println(JsonUtils.objectToJson(employeeMapper.findAll()));
 	}
 
 }
